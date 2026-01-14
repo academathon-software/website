dd8bfd06b6bb73f7d25f6620572a8be9ababe198
Ryan <ryant012015@gmail.com>
2026-01-14 14:33:09 -0500

    deleted old stuff

--- AcademathonWebApp-main/history/academathon/src2/src/ui/button.js
+++
@@ -1,9 +0,0 @@
-import './button.css'; // Import CSS for styling
-
-function Button({ text, onClick, type}) {
-  return (
-    <button className="button" type={type} onClick={onClick}>{text}</button>
-  );
-}
-
-export default Button;
--- AcademathonWebApp-main/src/firefunc/profileFuncs.js
+++
@@ -1,3 +0,0 @@
-import "./firebaseFuncs";
-
-
