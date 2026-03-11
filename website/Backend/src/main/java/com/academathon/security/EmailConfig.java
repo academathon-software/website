c7aeb2c6fa5445d5a4e73d31603203b159e8eef2
academathon-software <academathon-software@users.noreply.github.com>
2026-03-11 15:03:29 -0400

    fix email 12 hour time

--- website/Backend/src/main/java/com/academathon/security/EmailConfig.java
+++ website/Backend/src/main/java/com/academathon/security/EmailConfig.java
@@ -31,6 +31,8 @@ public class EmailConfig {
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.smtp.ssl.protocols", "TLSv1.2");
         props.put("mail.debug", "true");
 
         return mailSender;
